f6633db35f10a95380cc3771ae2a9385be84743f
jimmy0705 <junedsarwar@email.com>
2025-09-08 18:50:11 +0530

    todo unit test written

--- todo.js
+++ todo.js
@@ -1,55 +1,40 @@
 let todos = [];
-
 let idCounter = 1;
 
 function addTodo(title) {
-    const newTodo = { id: idCounter++, title: title };
-    todos.push(newTodo);
-    // console.log(todos);
-    return newTodo;
+  const newTodo = { id: idCounter++, title, completed: false };
+  todos.push(newTodo);
+  return newTodo;
 }
-
-addTodo('hello world');
-addTodo('practice oops js');
-addTodo('keep racticing to get the grip of coding as before');
 
 function getTodos() {
-    // console.log([...todos])
-    return [...todos]
+  return [...todos]; // Return a shallow copy
 }
 
-getTodos();
+function updateTodo(id, updates) {
+  const index = todos.findIndex(todo => todo.id === id);
+  if (index === -1) return null;
 
-
-function updateTodo(id, newTitle) {
-    const index = todos.findIndex(todo => todo.id === id);
-    console.log(index);
-    if (index === -1) return null;
-    console.log(todos[index]);
-    todos[index] = { ...todos[index], title: newTitle };
-    return todos[index];
+  todos[index] = { ...todos[index], ...updates };
+  return todos[index];
 }
-
-updateTodo(1, "always practice js and ts")
-// getTodos();
-console.log(getTodos())
 
 function deleteTodo(id) {
-    const index = todos.findIndex(todo => todo.id === id);
-    if (index === -1) return false;
-    todos.splice(index, 1);
-    return true;
+  const index = todos.findIndex(todo => todo.id === id);
+  if (index === -1) return false;
+  todos.splice(index, 1);
+  return true;
 }
 
 function clearTodos() {
-    todos = [];
-    idCounter = 1;
+  todos = [];
+  idCounter = 1;
 }
 
 module.exports = {
-    addTodo,
-    getTodos,
-    updateTodo,
-    deleteTodo,
-    clearTodos
+  addTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo,
+  clearTodos
 };
--- todo.test.js
+++ todo.test.js
@@ -4,9 +4,53 @@ const {
     updateTodo,
     deleteTodo,
     clearTodos
-} = require('./todo.test.js')
+} = require('./todo');
 
+describe("To-Do Manager", () => {
+    beforeEach(() => {
+        clearTodos();
+    });
 
-describe('todo testing', () => {
+    test("should add a new todo", () => {
+        const todo = addTodo("Learn Jest");
+        expect(todo).toHaveProperty("id", 1);
+        expect(todo).toHaveProperty("title", "Learn Jest");
+    });
 
+    test("should return all todos", () => {
+        addTodo("Task 1");
+        addTodo("Task 2");
+        const todos = getTodos();
+        expect(todos.length).toBe(2);
+        expect(todos[1].title).toBe("Task 2");
+    });
+
+    test("should update a todo", () => {
+        const todo = addTodo("Original Task");
+        const updated = updateTodo(todo.id, { title: "Updated Task", completed: true });
+        expect(updated.title).toBe("Updated Task");
+        expect(updated.completed).toBe(true);
+    });
+
+    test("should return null when updating non-existent todo", () => {
+        const result = updateTodo(999, { title: "Does not exist" });
+        expect(result).toBeNull();
+    });
+
+    test("should delete a todo", () => {
+        const todo = addTodo("To Delete");
+        const success = deleteTodo(todo.id);
+        expect(success).toBe(true);
+        expect(getTodos().length).toBe(0);
+    });
+
+    test("should return false when deleting non-existent todo", () => {
+        const success = deleteTodo(999);
+        expect(success).toBe(false);
+    });
+
+    test("should isolate data between tests", () => {
+        const todos = getTodos();
+        expect(todos.length).toBe(0);
+    });
 });
